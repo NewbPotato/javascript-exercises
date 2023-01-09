16fae180e6bc6bc80a4513ad7aa1df714ef575db
NewbPotato <alvin.leo.guarin@gmail.com>
2023-01-09 17:48:35 +0800

    finish up to exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach(element => {
+    sum += element;
+  });
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (a, b) => a * b )
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return (num1 ** num2);
 };
 
-const factorial = function() {
-	
+function factorial(n) {
+	if (n == 0 && n <= 1) {
+    return 1;
+  }
+  else {
+    return n * factorial(n-1);
+  }
 };
 
 // Do not edit below this line
