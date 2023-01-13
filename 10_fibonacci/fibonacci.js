e572e57ec78fada9872b15ed28fec354660bf559
NewbPotato <alvin.leo.guarin@gmail.com>
2023-01-13 15:43:26 +0800

    finished excercise 10, 11, and 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let fib1 = 0;
+    let fib2 = 1;
+    for (let i = 1; i < n; i++) {
+        let temp = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = temp;
+    }
+    return n < 0 ? "OOPS" : (n <= 1 ? 1 : fib2);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let date = new Date();
 
+    arr.forEach(element => {
+        if (element["yearOfDeath"] == null) {
+            element["yearOfDeath"] = date.getFullYear();
+        }
+        element["age"] = element["yearOfDeath"] - element["yearOfBirth"];
+    });
+    oldest = Math.max(...arr.map(o => o.age));
+    //Or oldest = Math.max.apply(Math, arr.map(obj => obj.age))
+    return arr.find(o => o.age == oldest);
 };
 
 // Do not edit below this line
